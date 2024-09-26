d334356067e146e2586676dd84bc38829187f2eb
morganjowitt <jowitt.morgan@gmail.com>
2024-09-26 15:35:54 +0200

    ajout de black pour le formatage du code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 import requests
 import jwt
 from datetime import datetime
@@ -9,6 +9,7 @@ app = FastAPI()
 service_start_time = datetime.now()
 request_count = 0
 
+
 @app.get("/health")
 def health_check():
     global request_count
@@ -17,11 +18,13 @@ def health_check():
         "status": "OK",
         "uptime": str(uptime),
         "requests_served": request_count,
-        "message": "The service is healthy and running."
+        "message": "The service is healthy and running.",
     }
 
-EXTERNAL_API_URL = "https://ensagri-pprd.api.agriculture.gouv.fr/arpent-resultats-api/etablissements" #problème d'access
-EXTERNAL_API_URL2= 'https://impactco2.fr/api/v1/chauffage'
+
+EXTERNAL_API_URL = "https://ensagri-pprd.api.agriculture.gouv.fr/arpent-resultats-api/etablissements"  # problème d'access
+EXTERNAL_API_URL2 = "https://impactco2.fr/api/v1/chauffage"
+
 
 @app.get("/data_gouv")
 def get_data_gouv():
@@ -32,11 +35,16 @@ def get_data_gouv():
     except requests.exceptions.RequestException as e:
         # Gérer les erreurs
         return {"error": "Failed to retrieve data", "details": str(e)}
-    
-def create_access_token(input: dict) -> str :
+
+
+def create_access_token(input: dict) -> str:
     to_encode = input.copy()
-    encoded_jwt = jwt.encode(to_encode, 'SECRET_KEY', )
+    encoded_jwt = jwt.encode(
+        to_encode,
+        "SECRET_KEY",
+    )
     return encoded_jwt
+
 
 # Endpoint pour générer le JWT
 @app.post("/token")
@@ -44,13 +52,15 @@ def generate_jwt(name: dict):
     access_token = create_access_token(name)
     return {"access_token": access_token, "token_type": "bearer"}
 
+
 # RESTE A CREER UNE FONCTION QUI UTILISE LE TOKEN avec jwt.decode
+
 
 @app.get("/fonction")
 def petite_fonction():
-    data_dict=get_data_gouv()
-    if 'data' in data_dict:
-        ecv_values = [float(item['ecv']) for item in data_dict['data']]
+    data_dict = get_data_gouv()
+    if "data" in data_dict:
+        ecv_values = [float(item["ecv"]) for item in data_dict["data"]]
         return sum(ecv_values) / len(ecv_values)
     else:
-        return []  
+        return []
